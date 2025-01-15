5208f3b14116e025e63db125e01cf325d470437a
HussamShah10 <145381883+HussamShah10@users.noreply.github.com>
2025-01-15 09:12:04 -0500

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,59 @@
+/*
+Welcome to JDoodle!
+
+You can execute code here in 88 languages. Right now you’re in the Java IDE.
+
+  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
+
+  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+
+  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+
+Want to change languages? Try the search bar up the top.
+*/
+
+public class MyClass {
+  public static void main(String args[]) {
+    int x = 3;
+    int y = 5;
+    int z = 15;
+    int a = 7;
+    if (z % 15 == 0){
+    System.out.println("FizzBuzz");}
+    else if (z % 5 ==0){
+        System.out.println("Buzz");}
+    else if (z % 3 == 0){
+         System.out.println("Fizz");}
+    else{
+        System.out.println("Monkey");}
+  
+      if (y % 15 == 0){
+    System.out.println("FizzBuzz");}
+    else if (y % 5 ==0){
+        System.out.println("Buzz");}
+    else if (y % 3 == 0){
+         System.out.println("Fizz");}
+    else{
+        System.out.println("Monkey");}
+  
+  
+      if (x % 15 == 0){
+    System.out.println("FizzBuzz");}
+    else if (x % 5 ==0){
+        System.out.println("Buzz");}
+    else if (x % 3 == 0){
+         System.out.println("Fizz");}
+    else{
+        System.out.println("Monkey");}
+  
+  
+      if (a % 15 == 0){
+    System.out.println("FizzBuzz");}
+    else if (a % 5 ==0){
+        System.out.println("Buzz");}
+    else if (a % 3 == 0){
+         System.out.println("Fizz");}
+    else{
+        System.out.println("Monkey");}
+  }
+}
